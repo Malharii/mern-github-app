183f089e22620e44bc8273b0f9950abba9d36491
Malharii <mrmalhari96@gmail.com>
2024-06-09 19:49:12 +0530

    create an github like app and install react vite and tailwind css and created login sign-up page and compmonents

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ExplorePage from "./pages/ExplorePage";
+import LikesPage from "./pages/LikesPage";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <div className="flex text-white">
+        <Sidebar />
+        <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/explore" element={<ExplorePage />} />
+            <Route path="/likes" element={<LikesPage />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/ExplorePage.jsx
@@ -0,0 +1,9 @@
+const ExplorePage = () => {
+  return (
+    <div>
+      <h1>Explore Page</h1>
+    </div>
+  );
+};
+
+export default ExplorePage;
