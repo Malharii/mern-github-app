4367d102d197272559feeb919a5e75a9d0724b17
Malharii <mrmalhari96@gmail.com>
2024-06-15 17:47:04 +0530

    call back url is update

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/api/auth/github/callback",
+      callbackURL:
+        "https://malhar-mern-github-app.onrender.com/api/auth/github/callback",
     },
     async function (accessToken, refreshToken, profile, done) {
       // asynchronous verification, for effect...
